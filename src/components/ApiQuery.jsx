d8229df93c6dd04491888b4dab6ac1428dbfbb99
Koki Sakai <likuor0528@gmail.com>
2022-03-24 15:13:00 -0700

    Added image list MUI

--- src/components/ApiQuery.jsx
+++ src/components/ApiQuery.jsx
@@ -1,10 +1,39 @@
 import { useEffect, useState } from 'react';
+// import { Link } from 'react-router-dom';
+
+import { makeStyles } from '@material-ui/core/styles';
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
+import ImageListItemBar from '@material-ui/core/ImageListItemBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  imageList: {
+    width: '80%',
+  },
+  img: {
+    width: '100%',
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+}));
 
 export const ApiQuery = (async) => {
   const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
   const API_URL = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${API_KEY}`;
   console.log(API_URL);
   const [newsList, setNewsList] = useState([]);
+  const classes = useStyles();
 
   useEffect(() => {
     getNews();
@@ -20,15 +49,38 @@ export const ApiQuery = (async) => {
   };
 
   return (
-    <div>
-      {newsList.map((item, index) => (
-        <div key={index}>
-          <a href={item.url}>
-            <p>{item.title}</p>
-            <img src={item.urlToImage} alt='urlToImage' />
-          </a>
-        </div>
-      ))}
+    <div className={classes.root}>
+      <ImageList className={classes.imageList}>
+        <ImageListItem key='Subheader' cols={2} style={{ height: 'auto' }}>
+          <ListSubheader component='div'>December</ListSubheader>
+        </ImageListItem>
+        {newsList.map((item, index) => (
+          <ImageListItem key={index} style={{ height: '20vw' }}>
+            {/* <Link to={item.url}> */}
+            {/* <a href={item.url}> */}
+            <img
+              className={classes.img}
+              src={item.urlToImage}
+              alt={item.title}
+            />
+            <ImageListItemBar
+              title={item.title}
+              subtitle={<span>{item.description}</span>}
+              actionIcon={
+                <IconButton
+                  aria-label={`info about ${item.title}`}
+                  className={classes.icon}
+                  href='#'
+                >
+                  <BookmarkIcon />
+                </IconButton>
+              }
+            />
+            {/* </a> */}
+            {/* </Link> */}
+          </ImageListItem>
+        ))}
+      </ImageList>
     </div>
   );
 };
